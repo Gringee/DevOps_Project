c61bee5f970ab25298d94d95bc0af71855bb59e0
Filip Kulig <52160738+Gringee@users.noreply.github.com>
2025-01-09 20:15:56 +0100

    Port change
    From 5001 to 5000

--- app/app.py
+++ app/app.py
@@ -145,4 +145,4 @@ def delete_task(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
